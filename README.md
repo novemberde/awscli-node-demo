6736c2f196ecac819e416c881b3ceec239f9b73c
novemberde@nate.com <novemberde@nate.com>
2018-09-04 11:31:20 +0900

    Add a script creating vpc

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # awscli-node-demo
 Demo node scripts using awscli
+
+## Requirements
+
+- Install AWS-CLI on your client.[https://docs.aws.amazon.com/ko_kr/cli/latest/userguide/installing.html](https://docs.aws.amazon.com/ko_kr/cli/latest/userguide/installing.html)
---
+++ scripts/vpc.js
@@ -0,0 +1,44 @@
+const execa = require('execa');
+const {Signale} = require('signale');
+const signal = new Signale();
+
+// const profile = "workshopUser";
+const profile = process.env.CLI_PROFILE;
+
+const main = async () => {
+    try {
+        signal.success(`Profile is ${profile}`);
+
+        const vpcResult = JSON.parse((await execa.shell(`aws ec2 create-vpc --cidr-block 10.0.0.0/16 --profile ${profile}`)).stdout);
+        signal.debug(vpcResult);
+        const vpcId = vpcResult.Vpc.VpcId;
+        signal.success("VPC is created");
+
+        await execa.shell(`aws ec2 create-subnet --vpc-id ${vpcId} --cidr-block 10.0.0.0/24 --profile ${profile}`)
+        signal.success("Subnet 1 is created");
+
+        await execa.shell(`aws ec2 create-subnet --vpc-id ${vpcId} --cidr-block 10.0.1.0/24 --profile ${profile}`)
+        signal.success("Subnet 2 is created");
+        
+        const internetGatewayResult = JSON.parse((await execa.shell(`aws ec2 create-internet-gateway --profile ${profile}`)).stdout)
+        signal.debug(internetGatewayResult);
+        const internetGatewayId = internetGatewayResult.InternetGateway.InternetGatewayId;
+        signal.success(`InternetGateway is created`);
+        signal.success(`InternetGatewayId: ${internetGatewayId}`);
+
+        await execa.shell(`aws ec2 attach-internet-gateway --vpc-id ${vpcId} --internet-gateway-id ${internetGatewayId} --profile ${profile}`)
+        signal.success("InternetGateway is attached on VPC");
+        
+        const routeTableResult = JSON.parse((await execa.shell(`aws ec2 create-route-table --vpc-id ${vpcId} --profile ${profile}`)).stdout)
+        const routeTableId = routeTableResult.RouteTable.RouteTableId;
+        signal.success("RouteTable is created");
+        
+        await execa.shell(`aws ec2 create-route --route-table-id ${routeTableId} --destination-cidr-block 0.0.0.0/0 --gateway-id ${internetGatewayId} --profile ${profile}`)
+        signal.success("Route is created");        
+    } catch (err) {
+        signal.error(err);
+        process.exit(1);
+    }
+}
+
+main();
