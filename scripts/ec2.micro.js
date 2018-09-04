bab0f9f5f838a7aa6a529753ee89d7a6226b9407
novemberde@nate.com <novemberde@nate.com>
2018-09-04 18:50:06 +0900

    Add a script creating ec2 micro

---
+++ scripts/ec2.micro.js
@@ -0,0 +1,69 @@
+const execa = require('execa');
+const {Signale} = require('signale');
+const signale = new Signale();
+
+const profile = "workshopUser";
+const asyncStdIn = () => new Promise((resolve, reject) => {
+    const p = process.stdin.on('data', data => {
+        const str = data.toString().split("\n")[0];
+        if(!str || str.length < 3) return;
+
+        str.trim();
+        resolve(str);
+    }).on('error', err => reject(err));
+});
+
+const main = async () => {
+    try {
+        if(!profile) throw new Error(`CLI_PROFILE should be defined`);
+        signale.success(`AWSCLI Profile: ${profile}`);
+
+        const describedVpcs = JSON.parse((await execa.shell(`aws ec2 describe-vpcs --profile ${profile}`)).stdout);
+        signale.info(describedVpcs);
+        
+        process.stdout.write("VPC ID: ");
+        const vpcId = await asyncStdIn();
+        
+        process.stdout.write("SecurityGroup name: ");
+        const securityGroupName = await asyncStdIn();
+
+        process.stdout.write("SecurityGroup description: ");
+        const securityGroupDesc = await asyncStdIn();
+        const sgResult = JSON.parse((await execa.shell(`aws ec2 create-security-group \
+        --group-name ${securityGroupName} --description ${securityGroupDesc} --vpc-id ${vpcId} \
+        --profile ${profile}`)).stdout);
+
+        const sgId = sgResult.GroupId;
+        await execa.shell(`aws ec2 authorize-security-group-ingress --group-id ${sgId} --protocol tcp --port 80 --cidr 0.0.0.0/0 --profile ${profile}`)
+        await execa.shell(`aws ec2 authorize-security-group-ingress --group-id ${sgId} --protocol tcp --port 22 --cidr 0.0.0.0/0 --profile ${profile}`)
+
+        process.stdout.write("Key Name: ");
+        const keyName = await asyncStdIn();
+        await execa.shell(`aws ec2 create-key-pair --key-name ${keyName} --query 'KeyMaterial' --output text > ${keyName}.pem --profile ${profile}`)
+        await execa.shell(`chmod 400 ${keyName}.pem`)
+
+        const describedSubnets = JSON.parse((await execa.shell(`aws ec2 describe-subnets --filters "Name=vpc-id,Values=${vpcId}" --query 'Subnets[*].{ID:SubnetId,CIDR:CidrBlock}' --profile ${profile}`)).stdout);
+        signale.info(describedSubnets);
+        
+        // Ubuntu image
+        const ec2Result = (await execa.shell(`aws ec2 run-instances --image-id ami-00ca7ffe117e2fe91 \
+        --subnet-id ${describedSubnets[0].ID} \
+        --security-group-ids ${sgId} \
+        --count 1 \
+        --instance-type t2.micro \
+        --key-name ${keyName} \
+        --query 'Instances[0].InstanceId' \
+        --profile ${profile}`)).stdout;
+
+        signale.success(`${ec2Result} is successfully created!`);
+        process.exit(0);
+    } catch (err) {
+        signale.error(err);
+        process.exit(1);
+    }
+}
+
+
+main();
+
+
--- scripts/vpc.js
+++ scripts/vpc.js
@@ -1,42 +1,43 @@
 const execa = require('execa');
 const {Signale} = require('signale');
-const signal = new Signale();
+const signale = new Signale();
 
 // const profile = "workshopUser";
 const profile = process.env.CLI_PROFILE;
 
 const main = async () => {
     try {
-        signal.success(`Profile is ${profile}`);
+        if(!profile) throw new Error(`CLI_PROFILE should be defined`);
+        signale.success(`AWSCLI Profile: ${profile}`);
 
         const vpcResult = JSON.parse((await execa.shell(`aws ec2 create-vpc --cidr-block 10.0.0.0/16 --profile ${profile}`)).stdout);
-        signal.debug(vpcResult);
+        // signale.info(vpcResult);
         const vpcId = vpcResult.Vpc.VpcId;
-        signal.success("VPC is created");
+        signale.success("VPC is created");
 
         await execa.shell(`aws ec2 create-subnet --vpc-id ${vpcId} --cidr-block 10.0.0.0/24 --profile ${profile}`)
-        signal.success("Subnet 1 is created");
+        signale.success("Subnet 1 is created");
 
         await execa.shell(`aws ec2 create-subnet --vpc-id ${vpcId} --cidr-block 10.0.1.0/24 --profile ${profile}`)
-        signal.success("Subnet 2 is created");
+        signale.success("Subnet 2 is created");
         
         const internetGatewayResult = JSON.parse((await execa.shell(`aws ec2 create-internet-gateway --profile ${profile}`)).stdout)
-        signal.debug(internetGatewayResult);
+        // signale.info(internetGatewayResult);
         const internetGatewayId = internetGatewayResult.InternetGateway.InternetGatewayId;
-        signal.success(`InternetGateway is created`);
-        signal.success(`InternetGatewayId: ${internetGatewayId}`);
+        signale.success(`InternetGateway is created`);
+        signale.success(`InternetGatewayId: ${internetGatewayId}`);
 
         await execa.shell(`aws ec2 attach-internet-gateway --vpc-id ${vpcId} --internet-gateway-id ${internetGatewayId} --profile ${profile}`)
-        signal.success("InternetGateway is attached on VPC");
+        signale.success("InternetGateway is attached on VPC");
         
         const routeTableResult = JSON.parse((await execa.shell(`aws ec2 create-route-table --vpc-id ${vpcId} --profile ${profile}`)).stdout)
         const routeTableId = routeTableResult.RouteTable.RouteTableId;
-        signal.success("RouteTable is created");
+        signale.success("RouteTable is created");
         
         await execa.shell(`aws ec2 create-route --route-table-id ${routeTableId} --destination-cidr-block 0.0.0.0/0 --gateway-id ${internetGatewayId} --profile ${profile}`)
-        signal.success("Route is created");        
+        signale.success("Route is created");        
     } catch (err) {
-        signal.error(err);
+        signale.error(err);
         process.exit(1);
     }
 }
